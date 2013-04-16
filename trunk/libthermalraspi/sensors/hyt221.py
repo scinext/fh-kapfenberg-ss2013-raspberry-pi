73622b2b67988511a911995e5ed6793a6378fb0d
robert.puerer@gmail.com <robert.puerer@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-04-16 18:53:57 +0000

    Added abstract class "Hybridsensor" and an implementation of the Hyt221 temperature/humidity sensor and an alternative implementation with the SMBus library

---
+++ trunk/libthermalraspi/sensors/hyt221.py
@@ -0,0 +1,46 @@
+from libthermalraspi.i2c_device import I2CDevice
+from libthermalraspi.sensors.hybridsensor import HybridSensor
+
+import time
+import struct
+
+class Hyt221(I2CDevice, HybridSensor):
+    def __init__(self, bus, addr):
+        I2CDevice.__init__(self, bus, addr)
+
+    def _perform_measurement(self):
+        # init -> trigger measurement
+        self.write('')
+        
+        # wait until measurement is completed
+        status = struct.unpack('B', self.read(1))
+        while(status[0] & 0xC0 != 0): # check the status bits
+            time.sleep(0.001)
+            status = struct.unpack('B', self.read(1))
+
+    def get_temperature(self):
+        self._perform_measurement()
+        
+        # read measurement results
+        result = self.read(4)
+        _, _, b2, b3 = struct.unpack('BBBB', result)
+        
+        # calculating temperature -> see datasheet
+        b3 = b3 & 0x3F
+        Traw = b2 << 6 | b3
+        T = 165.0 * Traw / (2**14) - 40
+        
+        return T    
+    def get_humidity(self):
+        self._perform_measurement()
+        
+        # read measurement results
+        result = self.read(2)
+        b0, b1 = struct.unpack('BB', result)
+        
+        Hraw = b0 << 8 | b1
+        Hraw = Hraw & 0x3FFF
+        H = 100.0 * Hraw / (2**14)
+        
+        return H
+    
---
+++ trunk/programs/hyt221-test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from libthermalraspi.sensors.hyt221 import Hyt221
+from libthermalraspi.sensors.hyt221_smbus import Hyt221SMBus
+
+
+hyt1 = Hyt221(1, 0x28)
+print "read/write implementation:"
+print("Temperature: %s" % hyt1.get_temperature())
+print("Humidity: %s" % hyt1.get_humidity())
+
+hyt2 = Hyt221(1, 0x28)
+print "smbus implementation:"
+print("Temperature: %s" % hyt2.get_temperature())
+print("Humidity: %s" % hyt2.get_humidity())
+
