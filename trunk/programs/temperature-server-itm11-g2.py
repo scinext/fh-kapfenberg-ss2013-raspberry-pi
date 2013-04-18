e885d16abbe0fed3b9951f33c123c12273736794
florian.stoppacher@gmail.com <florian.stoppacher@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-04-18 11:54:58 +0000

    server sends every second a new temperature with timestamp

--- trunk/programs/temperature-server-itm11-g2.py
+++ trunk/programs/temperature-server-itm11-g2.py
@@ -1,6 +1,9 @@
 #! /usr/bin/python
 
-import sys, socket
+import sys, socket, time, datetime
+
+from libthermalraspi.sensors.simulation import CyclicThermometer
+th = CyclicThermometer([1, 2.5, 3])
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((socket.gethostname(), 7000))
@@ -12,11 +15,13 @@ while 1:
     (clientsocket, address) = serversocket.accept()
 
     print("Received connection")
+
+    i = 0
+    while i < 1:
+        #data = clientsocket.recv(1024)
+        ts = time.time()
+        clientsocket.sendall(str(datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')) + ": " + str(th.get_temperature()) + "\n")
+        time.sleep(1)
     
-    data = ""
-    while data != "exit":
-        data = clientsocket.recv(1024)
-        print(data)
-    
-    clientsocket.close()
-    print("Connection closed")
+    #clientsocket.close()
+    #print("Connection closed")
