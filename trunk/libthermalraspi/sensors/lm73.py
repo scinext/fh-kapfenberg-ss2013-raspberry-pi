aa5f78df0b9600564fbbce4980200859bb02afc7
heintje6@gmail.com <heintje6@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-04-19 18:30:59 +0000

    bugfix: fixed shift error. no temp > 30 was possible

--- trunk/libthermalraspi/sensors/lm73.py
+++ trunk/libthermalraspi/sensors/lm73.py
@@ -10,10 +10,7 @@ class LM73(I2CDevice):
     def get_temperature(self):
         # request temperature
         self.write('\x00')
-        # read temperature - 2 Bytes
+        # read temperature
         result = self.read(2)
-	b1, b2 = struct.unpack('bb', result)
-	# shift first byte one position to the left and second 3 to the right
-	# &15 and | are used for binary addition of the two bytes
-	Traw = ((b1 & 15) << 1) | ((b2 & 15) >> 3)
-	return float(Traw)
+	b1, b2 = struct.unpack('BB', result)
+	return float((b1 << 1) | (b2 >> 7))
