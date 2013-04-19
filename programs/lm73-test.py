742218616419c6fa08ba798e38568da80c73e99c
heintje6 <heintje6@gmail.com>
2013-04-19 14:26:20 +0000

    corrected temp values and added a second sensor

--- programs/lm73-test.py
+++ programs/lm73-test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 from libthermalraspi.sensors.lm73 import LM73
 import os
@@ -6,18 +6,24 @@ import time
 
 # create LM73 object with the bus and device-address
 dev1 = LM73(1, 0x48)
+dev2 = LM73(1, 0x49)
 
 # loopcounter
 run = 1
 temp_sum = 0
+temp_avg = 0
 while True:
-        temp = dev1.get_temperature()
-        temp_sum += temp
+        temp1 = dev1.get_temperature()
+	temp2 = dev2.get_temperature()
+        temp_avg = (temp1+temp2)/2
+	temp_sum += temp_avg
         os.system('clear')
         print '############# I2C Temp Device #############'
         print '#                                         #'
-        print '#     Temperatur ( %05d' % run,'):     ', temp, '    #'
-        print '#     Durchschnittstemperatur:  ', round(temp_sum/run,1), '    #'
+        print '#     Temperatur-1   ( %05d' % run,'):  ', temp1, '   #'
+	print '#     Temperatur-2   ( %05d' % run,'):  ', temp2, '   #'
+	print '#     Temperatur-AVG ( %05d' % run,'):  ', temp_avg, '   #'
+        print '#     Durchschnittstemperatur :  ', round(temp_sum/run,1), '   #'
         print '#                                         #'
         print '###########################################'
         time.sleep(1)
