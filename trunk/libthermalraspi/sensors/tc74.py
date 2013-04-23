5ba592858120333928606315ee239b96e3cc20ad
Hippiebraut1@sms.at <Hippiebraut1@sms.at@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-04-23 18:37:14 +0000

    Maierhofer/Pecovnik: sensors/tc74.py unsupported operand types tuple and tuple; programs/tc74_test.py

--- trunk/libthermalraspi/sensors/tc74.py
+++ trunk/libthermalraspi/sensors/tc74.py
@@ -1,21 +1,26 @@
+#!/usr/bin/python
 from thermometer import Thermometer
 from libthermalraspi.i2c_device import I2CDevice
 import struct
 
 class TC74Thermometer (I2CDevice, Thermometer):
-    def __init__(self, bus, addr):
-        I2CDevice.__init__(self, bus, addr)
- def get_temperature(self):
-        # "temperature register" according to datasheet (DS)
 
+	def __init__ (self, bus, addr):
+		I2CDevice.__init__(self, bus, addr)
+
+	def get_temperature(self):
+
+# "temperature register" according to datasheet (DS)
 #response of  TC74 is only one byte, no bits must be shifted.
-self.write('\x00')
-self.write('')
-#Attention it doesn't works with sending a 0-Byte. You must send a empty Datapackage (Adress + no data) 
-tByte = self.read(1)
-tRaw = struct.unpack('B', tByte)
-#accounting of the temperature based on the temperature table.
-temp = tRaw
-if(tRaw > 127):
-    temp = (255 - tRaw) * (-1)
 
+		self.write('\x00')
+		self.write('')
+
+#Attention it doesn't works with sending a 0-Byte. You must send a empty Datapackage (Adress + no data) 
+		tByte = self.read(1)
+		tRaw = struct.unpack('B', tByte)
+
+#accounting of the temperature based on the temperature table.
+		temp = tRaw
+		if(tRaw > (0,127)):
+			    temp = ((0,255) - tRaw) * (-1)
