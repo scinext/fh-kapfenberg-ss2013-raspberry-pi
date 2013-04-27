9f0be06422327fbddb21f0f8cc51bc569637e7e2
ludvaig <ludvaig@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-04-27 09:08:50 +0000

    * removed project again due to svn client issues

--- android/TemperatureVisualizer/gen/at/fh/joanneum/sys/prog/linux/android/temperaturevisualizer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package at.fh.joanneum.sys.prog.linux.android.temperaturevisualizer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
