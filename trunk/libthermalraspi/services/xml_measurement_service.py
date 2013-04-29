1469bf285e0b16078f03d24caab50d32a7919fee
groeller.patrick@gmail.com <groeller.patrick@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-04-29 19:52:08 +0000

    impl of xmlservice, sometimes i hate svn...

--- trunk/libthermalraspi/services/xml_measurement_service.py
+++ trunk/libthermalraspi/services/xml_measurement_service.py
@@ -1,54 +1,25 @@
-from libthermalraspi.services.xml_measurement_service import XmlMeasurementService
-from xml.dom.minidom import parseString
+from xml.dom import minidom
+from databaseaccess.Measurement import Measurement
 
-import sys,os
-import unittest
+class XmlMeasurementService:
 
-class XmlMeasurementServiceTest(unittest.TestCase):
-    def test_toXml(self):
-        file = open(os.path.dirname(__file__)+os.sep+'resources'+os.sep+'measurement.xml','r')
-        data = file.read()
-        file.close()
-        dom = parseString(data)
-        expectedOutput =dom.toxml(encoding='utf-8')[:38]+ os.linesep + dom.toxml(encoding='utf-8')[38:]+ os.linesep
+    def __init__(self, listOfMeasurements):
+        self.__listOfMeasurements = listOfMeasurements
 
 
-        dataList =[MockObject(MockSensor("Strawberry"),1367168460,20.12,0),
-                   MockObject(MockSensor("Raspberry"),1367168460,30,0),
-                   MockObject(MockSensor("Banana"),1367168467,27.132,1),
-                   MockObject(MockSensor("Blackberry"),1367168461,27.132,0)]
-        xmlService = XmlMeasurementService(dataList)
-        self.assertEqual(expectedOutput, xmlService.toXml())
+    def toXml(self):
+        doc = minidom.Document()
+        root = doc.createElement("response")
+        doc.appendChild(root)
+        for measurement in self.__listOfMeasurements:
+            #if type(measurement) is Measurement:
+            item = doc.createElement('item')
+            root.appendChild(item)
+            item.setAttribute('name',measurement.getSensorname())
+            item.setAttribute('value',str(measurement.getMeasureVal()))
+            item.setAttribute('timestamp',str(measurement.getTimestamp()))
+            item.setAttribute('errorCode',str(measurement.getErrorCode()))
+
+        #print doc.toprettyxml(encoding='utf-8')
+        return doc.toprettyxml(encoding='utf-8')
         pass
-
-suite = unittest.defaultTestLoader.loadTestsFromTestCase(XmlMeasurementServiceTest)
-
-if __name__ == '__main__':
-    unittest.TextTestRunner().run(suite)
-    pass
-
-class MockObject:
-    def __init__(self,sensor,timestamp,measureVal,errorCode):
-        self._sensor = sensor
-        self._timestamp = timestamp
-        self._measureVal = measureVal
-        self._errorCode = errorCode
-
-    def getSensorname(self):
-        return self._sensor.getName()
-
-    def getTimestamp(self):
-        return self._timestamp
-
-    def getMeasureVal(self):
-        return self._measureVal
-
-    def getErrorCode(self):
-        return self._errorCode
-
-class MockSensor:
-     def __init__(self,name):
-         self.__name=name
-
-     def getName(self):
-          return self.__name
