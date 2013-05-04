6fb79e5d98fe8cc67d4bb80db2733fd5c3da283b
bernadette.pecovnik@gmail.com <bernadette.pecovnik@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-05-04 18:37:13 +0000

    CompositeSensor - test for getting average temperature of subprocesses

---
+++ trunk/libthermalraspi/unittests/test_compositeSensor_g2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from libthermalraspi.sensors.simulation import CyclicThermometer
+from libthermalraspi.sensors.thermometer import Thermometer
+import os
+import sys
+
+_listSensors = []
+_listSensors.append(CyclicThermometer([5]))
+_listSensors.append(CyclicThermometer([10]))
+_listSensors.append(CyclicThermometer([45]))
+
+_listTemperatures = []
+
+_childProcesses = []
+
+_r, _w = os.pipe()
+
+for listSensor in _listSensors:
+    print("next sensor") 
+    newpid = os.fork()
+    if newpid == 0: # parent process
+       os.close(_r)
+       _w = os.fdopen(_w, 'w')
+       print "child: writing - " + str(listSensor.get_temperature())
+       _w.write(str(listSensor.get_temperature())+ "\n")
+       _w.close()
+       print "child: closing"
+       os._exit(0)
+    else:
+       _childProcesses.append(newpid)           
+
+os.close(_w)
+_r = os.fdopen(_r) # turn r into a file object
+for childProcess in _childProcesses:
+   os.waitpid(childProcess, 0)
+   temperature = _r.readline()
+   print "parent: reading from " + str(childProcess) + " => temperature is " + temperature  
+   _listTemperatures.append(temperature)
+   
+_listTemperatures = map(float, _listTemperatures)
+avgTemperature = sum(_listTemperatures) / len(_listTemperatures)
+
+print "***************************************"
+print "Durchschnittstemperatur betraegt: " + str(avgTemperature)
+print "***************************************"
+            
