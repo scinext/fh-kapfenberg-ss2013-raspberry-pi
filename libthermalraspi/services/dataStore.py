b85a605c2fb696a6e1f206957b1fad8a0fc29879
jf <jf@faschingbauer.co.at>
2013-05-05 06:06:30 +0000

    move over files from accidental commit (data logger database, Robert Angerer)

---
+++ libthermalraspi/services/dataStore.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from databaseaccess.SensorDAO import SensorDAO
+from databaseaccess.MeasurementDAO import MeasurementDAO
+
+class DataStore(object):
+    """Verbindungsklasse zur Abstraktion des Datenbankzugriffs."""
+
+    def __init__(self):
+        pass
+
+    def get_sample(self, fromDatetime, toDatetime, maxResultCount = None, sensorIDs = None):
+        """Anzahl von Messwerten eines Zeitraumes aus der Datenbank auslesen.
+           Parameter:
+           fromDatetime   -- Timestamp, Anfangszeitpunkt der Messwerte
+           toDatetime     -- Timestamp, Endzeitpunkt der Messwerte
+           maxResultCount -- int, maximale Anzahl der auszulesenden Messwerte - Standardwert: None
+           sensorIDs      -- int[], Liste der auszulesenden SensorenIDs wie in der Datenbank gespeichert - Standardwert: None
+        """
+        return SensorDAO.readMeasurements(fromDatetime, toDatetime, maxResultCount, sensorIDs)
+        pass
+     
+    def add_sample(self, timestamp, sensorname, temperatur, status):
+        """Messwert eines bestimmten Sensors in die Datenbank schreiben.
+           Parameter:
+           timestamp    -- Timestamp, Zeitstempel der Uhrzeit des Messwertes
+           sensorname   -- String, Bezeichnung des messenden Sensors 
+           temperatur   -- float, Wert der gemessenen Temperatur
+           status       -- int, Status des Sensors
+        """
+        sample = MeasurementDAO(sensorname, timestamp, temperatur, status)
+        sample.insert()
+        pass
+    
