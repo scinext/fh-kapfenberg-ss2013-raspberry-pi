4ef4598e43bc5435fba6f7e65dfd6dd600f7f336
toolbone@googlemail.com <toolbone@googlemail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-05-11 08:57:43 +0000

    Anpassung sampleCollector-Interface und parallelSampleCollector zur Verwendung eines externen Loopers sowie Übergabe einer Datenbank-Connection und eines Dictonary von Sensoren (SensorName : SensorInstanz).

--- trunk/libthermalraspi/services/sampleCollector.py
+++ trunk/libthermalraspi/services/sampleCollector.py
@@ -6,20 +6,21 @@ class SampleCollector(object):
     __metaclass__ = abc.ABCMeta
     
     @abc.abstractmethod
-    def configuration(self, sensorList=[] ):
-        """Sammler der Messdaten mit Liste aller abzufragenden Sensoren versorgen."""
-        return
-    
-    @abc.abstractmethod
-    def run(self, timeDelta):
-        """Messungen starten und Zeitintervall zwischen zwei Abfragen
-        der Sensormesswerte in Millisekunden festlegen. Messungen erfolgen, bis stop()
-        aufgerufen wird."""
-        return
-    
-    @abc.abstractmethod
-    def stop(self):
-        """Abbruchbedingung aller weiterer Messungen setzen."""
-        return
+    def __init__(self, store, sensorList = {}):
+        """Collector mit Datenspeicher und Dictionary der abzufragender
+        Sensoren übergeben.
         
+        @param store: Datenbank-Connection
+        @param sensorList: Dictionary SensorName : SensorInstanz
+        """
+        return        
+    
+    @abc.abstractmethod
+    def run(self, looper):
+        """Messungen starten und per externem iterierbarem Objekt
+        (looper) Intervall der Messzyklen festlegen.
+        
+        @param looper: Iterierbares Objekt/List/...
+        """
+        return        
 
