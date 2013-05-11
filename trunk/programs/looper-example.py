91be50f21a40e6ee920d66fd65df578ccb2138c2
jf@faschingbauer.co.at <jf@faschingbauer.co.at@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-05-11 08:48:52 +0000

    looper example

--- trunk/programs/looper-example.py
+++ trunk/programs/looper-example.py
@@ -6,13 +6,15 @@ class SleepingLooper:
     def __init__(self, interval_seconds):
         self.__interval = interval_seconds
         self.__counter = 0
+
+        # to be set from signal handler
+        self.__stop = False
         
     def __iter__(self):
-        while True:
+        while not self.__stop:
             yield self.__counter
             self.__counter += 1
             time.sleep(self.__interval)
-
 
 looper = SleepingLooper(1.5)
 for i in looper:
