1c094f53dc9e1b586769699aa508fc535300deb9
mariopflegpeter <MarioPflegpeter@gmail.com>
2013-05-11 15:09:55 +0000

    group 2 - team pflegp, prutsm, steinb, winste - new class SensorConfigReader - new unittest for SensorConfigReader
    The SensorConfigReader reads the sensor.cfg file as a dictionary

---
+++ libthermalraspi/services/SensorConfigReader.py
@@ -0,0 +1,25 @@
+'''
+Created on May 11, 2013
+
+@author: group 2 - team pflegp, prutsm, steinb, winste
+'''
+import os
+from libthermalraspi.sensors.lm73device import LM73Device
+from libthermalraspi.sensors.ad7414 import AD7414Thermometer
+from libthermalraspi.sensors.tc74 import TC74Thermometer
+from libthermalraspi.sensors.hyt221 import Hyt221
+
+class SensorConfigReader(object):
+    def __init__(self, defaultPath = os.path.join( os.path.dirname( os.path.abspath( __file__ ) ), "sensor.cfg") ):
+        self._filePath = defaultPath
+        
+    def read(self):
+        try:
+            return eval( file( self._filePath ).read(), { 'LM73'   : LM73Device
+                                                         ,'AD7414' : AD7414Thermometer
+                                                         ,'TC74'   : TC74Thermometer
+                                                         ,'Hyt221' : Hyt221 } )
+        except IOError as e:
+            print "IOError: %s" % e
+        except Exception as e:
+            print "Unknown error: %s" % e
