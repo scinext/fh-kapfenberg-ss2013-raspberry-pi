67266a75ef8a39121546f09549d5553387a712cb
neubauerm <neubauerm@gmail.com>
2013-05-14 21:05:11 +0000

    test program for composite sensor Group3

--- libthermalraspi/unittests/easy_suite.py
+++ libthermalraspi/unittests/easy_suite.py
@@ -1,7 +1,7 @@
 import test_simulation
 import test_lm73device
 import test_xmlMeasurementService
-import test_compositeThermometer_SWD11G2_3
+import test_compositeSensor_g2_3
 import test_compositeThermometer_SWD11G2_2
 import unittest
 
@@ -9,7 +9,7 @@ suite = unittest.TestSuite()
 suite.addTest(test_simulation.suite)
 suite.addTest(test_lm73device.suite)
 suite.addTest(test_xmlMeasurementService.suite)
-suite.addTest(test_compositeThermometer_SWD11G2_3.suite)
+suite.addTest(test_compositeSensor_g2_3.suite)
 suite.addTest(test_compositeThermometer_SWD11G2_2.suite)
 
 if __name__ == '__main__':
