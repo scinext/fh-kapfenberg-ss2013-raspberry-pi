61dd8f1d6e011bc756570bc1b1b8f77ec4745292
rossmann.dominik <rossmann.dominik@gmail.com>
2013-05-16 08:27:21 +0000

    ...

--- libthermalraspi/sensors/thermo_proxy_itmG2.py
+++ libthermalraspi/sensors/thermo_proxy_itmG2.py
@@ -2,25 +2,16 @@ from thermometer import Thermometer
 import socket
 
 class ThermoProxy_ItmG2(Thermometer):
-	
-	def __init__(self, host, port):
-		self.__host = host
-		self.__port = port
-           
-            
-	def est_conn(self):
-		self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.__s.connect((self.__host, self.__port))
-		
-    def start(self):
-        Connection(self._s).start()
-        
-    def close(self):
-        self._s.close()
-    
+
+    def __init__(self):
+        self.__host = '127.0.0.1'
+        self.__port = 7000
+
+    def get_temperature(self):
+        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.connect((self.__host, self.__port))
+        self.send_msg('get_temperature')
+        return self.__s.recv(1024)
+
     def send_msg(self, msg):
-        return self._s.send(msg)
-        
-		
-		
-		
+        return self.__s.send(msg)
