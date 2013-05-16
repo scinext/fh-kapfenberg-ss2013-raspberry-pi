876fd38b8dba2e5693cc9fcca7ca5340e2d6a0fe
jf@faschingbauer.co.at <jf@faschingbauer.co.at@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-05-16 08:38:20 +0000

    *** empty log message ***

--- trunk/programs/temperature-server-itm11-g2.py
+++ trunk/programs/temperature-server-itm11-g2.py
@@ -3,7 +3,10 @@
 import sys, socket, time, datetime
 
 from libthermalraspi.sensors.simulation import CyclicThermometer
-th = CyclicThermometer([1, 2.5, 3])
+from libthermalraspi.sensors.stds75 import Stds75
+
+## th = CyclicThermometer([1, 2.5, 3])
+th = Stds75(1, 0x4e)
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((socket.gethostname(), 7000))
