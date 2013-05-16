bbb1bbfb40c866caf7d43d8f7654391a935360e4
dag0310@gmail.com <dag0310@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-05-16 08:28:47 +0000

    Client works with server. Can send messages and receive the answer.

--- trunk/programs/thermometer-client-itm11-g1.py
+++ trunk/programs/thermometer-client-itm11-g1.py
@@ -1,19 +1,16 @@
 import sys, socket, threading
 
-class ThermoProxy:
+class ThermoProxy():
     """Connects with a server"""
     
-    def __init__(self, host="localhost", port=1234):
+    def __init__(self, host="127.0.0.1", port=1024):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self._sock.connect((host, port))
+            server = self._sock.getpeername()[0] + ":" + str(self._sock.getpeername()[1])
+            print("%s connected" % (server));
         except:
-            print("No connection possible!")
-
-    def start(self):
-        print("Client started...")
-        Connection(self._sock).start()
-        #self._sock.close()
+            print("Can not reach server.")
     
     def send_msg(self, msg):
         self._sock.send(msg)
@@ -21,34 +18,30 @@ class ThermoProxy:
     def receive_msg(self):
         return self._sock.recv(1024)
 
-class Connection(threading.Thread):
-    def __init__(self, conn):
-        threading.Thread.__init__(self)
-        self.conn = conn
-        self._server = self.conn.getpeername()[0] + ":" + str(self.conn.getpeername()[1])
-        print("Server %s connected..." % (self._server)) 
-        
-    def run(self):
-            data = self.conn.recv(1024)
-            print("%s" % (data))
-
-HOST = "10.63.26.24"
-PORT = 1024
-
-if (len(sys.argv) > 1):
-    HOST = sys.argv[1]
-if (len(sys.argv) > 2):
-    PORT = int(sys.argv[2])
-
 if __name__ == '__main__':
-    tp = ThermoProxy(HOST, PORT)
-    tp.start()
+    default = "127.0.0.1:1024"
+    host_str = raw_input("<HOST>:<PORT> [%s] " % (default))
     
-    # Send example data
-    #tp.send_msg('HELP')
-    #print(tp.receive_msg)
-    #tp.send_command('GET_TEMP')
-    #tp.send_command('BYE')
-    #for i in range(1, 11):
-    #    print("Value %d: %s degree C" % (i, tp.send_command('GET_TEMP')))
-    #tp.send_command('BYE')
+    # use default address if not specified
+    if (host_str == ''):
+        host_str = default
+    
+    host_str_array = host_str.split(":")
+    
+    # use specified host address
+    HOST = host_str_array[0]
+    
+    if len(host_str_array) > 1:
+        # use specified port
+        PORT = int(host_str_array[1])
+    else:
+        # use default port
+        PORT = int(default.split(":")[1])
+    
+    tp = ThermoProxy(HOST, PORT)
+    print(tp.receive_msg())
+    
+    while True:
+        cmd = raw_input("client# ")
+        tp.send_msg(cmd.upper())
+        print(tp.receive_msg())
