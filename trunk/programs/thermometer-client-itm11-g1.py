26ca1842e4b92b7104f60a9ba3f4e1eaa5d046f7
dag0310@gmail.com <dag0310@gmail.com@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-05-16 08:53:06 +0000

    Close program if the server closes the connection (break while-True-loop)

--- trunk/programs/thermometer-client-itm11-g1.py
+++ trunk/programs/thermometer-client-itm11-g1.py
@@ -1,10 +1,11 @@
-import sys, socket, threading
+import sys, socket
 
 class ThermoProxy():
     """Connects with a server"""
     
     def __init__(self, host="127.0.0.1", port=1024):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
         try:
             self._sock.connect((host, port))
             server = self._sock.getpeername()[0] + ":" + str(self._sock.getpeername()[1])
@@ -20,11 +21,11 @@ class ThermoProxy():
 
 if __name__ == '__main__':
     default = "127.0.0.1:1024"
-    host_str = raw_input("<HOST>:<PORT> [%s] " % (default))
+    
+    host_str = raw_input("client# <HOST>:<PORT> [%s] " % (default))
     
     # use default address if not specified
-    if (host_str == ''):
-        host_str = default
+    if (host_str == ''): host_str = default
     
     host_str_array = host_str.split(":")
     
@@ -42,6 +43,22 @@ if __name__ == '__main__':
     print(tp.receive_msg())
     
     while True:
-        cmd = raw_input("client# ")
+        # prefix that will be shown as long as you are connected to the server
+        prefix = "client@%s# " % (tp._sock.getpeername()[0])
+        
+        # let the client set a command
+        cmd = raw_input(prefix)
+        
+        # send message
         tp.send_msg(cmd.upper())
-        print(tp.receive_msg())
+        
+        # receive message
+        rcv = tp.receive_msg()
+        
+        # end program if connection closed by server
+        if not rcv: break
+        
+        # print the received message of the server
+        print(rcv)
+    
+    print("--- END OF PROGRAM ---")
