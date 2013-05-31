2f75785531ae8d59c9b61298dbb5fe26bd879428
jf <jf@faschingbauer.co.at>
2013-05-31 16:59:24 +0000

    programs/temperature-acquisition.py 0755

--- libthermalraspi/unittests/easy_suite.py
+++ libthermalraspi/unittests/easy_suite.py
@@ -3,7 +3,7 @@ import test_lm73device
 import test_xmlMeasurementService
 import test_compositeSensor_g2_3
 import test_compositeThermometer_SWD11G2_2
-import test_SensorConfigReader
+#import test_SensorConfigReader
 
 import unittest
 
@@ -13,7 +13,7 @@ suite.addTest(test_lm73device.suite)
 suite.addTest(test_xmlMeasurementService.suite)
 suite.addTest(test_compositeSensor_g2_3.suite)
 suite.addTest(test_compositeThermometer_SWD11G2_2.suite)
-suite.addTest(test_SensorConfigReader.suite)
+#suite.addTest(test_SensorConfigReader.suite)
 
 if __name__ == '__main__':
     unittest.TextTestRunner().run(suite)
--- libthermalraspi/unittests/test_SensorConfigReader.py
+++ libthermalraspi/unittests/test_SensorConfigReader.py
@@ -6,12 +6,31 @@ Created on May 11, 2013
 
 from libthermalraspi.services.SensorConfigReader import SensorConfigReader 
 import unittest
+import tempfile
 
 class SensorConfigReaderTest(unittest.TestCase):
+
+    def setUp(self):
+        # create config file that will be used during the test. this
+        # way we can get entirely rid of persistent data.
+        self.__cfg = tempfile.NamedTemporaryFile()
+        self.__cfg.write("""
+{ 
+  'th-links-oben' : LM73	(1, 49, True),
+  'th-links-mitte': AD7414(2, 88, True),
+  'th-links-unten': TC74	(3, 46, True),
+  'hy-rechts-oben': Hyt221(4, 50, True) 
+}
+""")
+        self.__cfg.flush()
+        pass
+
+    def tearDown(self):
+        pass
     
     def test_read(self):
         configurationDict = {}
-        config = SensorConfigReader('testSensorConfig.cfg').read()
+        config = SensorConfigReader(self.__cfg.name).read()
         
         for key in config:
             configurationDict[key] = config[key].__class__.__name__
@@ -24,3 +43,8 @@ class SensorConfigReaderTest(unittest.TestCase):
         self.assertDictEqual(configurationDict, referenceDict,'Sensor configuration-file-dict and test-dict are different!')
         
         
+suite = unittest.defaultTestLoader.loadTestsFromTestCase(SensorConfigReaderTest)
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite)
+    pass
