c9fbac0ad6e92c89eb6248a39664a8839b3681b1
jf <jf@faschingbauer.co.at>
2013-06-01 09:23:06 +0000

    *** empty log message ***

--- programs/temperature-acquisition.py
+++ programs/temperature-acquisition.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python
-from optparse import OptionParser
+
 from libthermalraspi.database import DataStoreInMemory
 from libthermalraspi.services.SensorConfigReader import SensorConfigReader
 from libthermalraspi.services.parallelSampleCollector import ParallelSampleCollector
 from libthermalraspi.programlooper import ProgramLooper
 from libthermalraspi.database.DataStoreSQL import DataStoreSQL
 
+from optparse import OptionParser
 import sqlite3
-
-# parameters: to be parsed from the commandline using argparse
-#DUMMY = False
-#DBFILE = "/tmp/some.db" # used if not DUMMY
-#PORT = 2345
-#SENSORS = "/tmp/sensors.cfg"
-#db = sqlite3.connect(DBFILE)
-#store = DataStoreSQLite(db)
 
 # instantiate TempServer as see fit, and run it
 parser = OptionParser()
@@ -22,8 +15,6 @@ parser.add_option("-f", "--dbfile",
                   action="store", type="string", dest="DBFILE", default="/tmp/some.db", help="used sqlite file name (default=/tmp/some.db)")
 parser.add_option("-s", "--sensors", 
                   action="store", type="string", dest="SENSORS", default="/tmp/sensors.cfg", help="sensor config file path (default=/tmp/sensors.cfg)")
-parser.add_option("-p", "--port",
-                  action="store", type="int", dest="PORT", default=2345, help="used port (default=2345)")
 parser.add_option("-i", "--interval",
                   action="store", type="int", dest="INTERVAL", default=3, help="used interval in seconds(default=3)")
 parser.add_option("-d", "--dummy",
@@ -31,7 +22,7 @@ parser.add_option("-d", "--dummy",
 
 (options, args) = parser.parse_args()
 
-print("Config: dummy=%s, dbfile=%s, port=%s, interval=%s, sensorconfig=%s" % (options.DUMMY, options.DBFILE, options.PORT, options.INTERVAL, options.SENSORS))
+print("Config: dummy=%s, dbfile=%s, interval=%s, sensorconfig=%s" % (options.DUMMY, options.DBFILE, options.INTERVAL, options.SENSORS))
 
 if options.DUMMY:
     store = DataStoreInMemory()
