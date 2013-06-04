6dd1f5e56deb6700bf3dd598ac4a3861ed578118
mkxsoft <mkxsoft@gmail.com>
2013-06-04 10:01:19 +0000

    Noch in Arbeit!

---
+++ libthermalraspi/unittests/test_ParallelSampleCollector.py
@@ -0,0 +1,41 @@
+
+# 04.06.: File noch in Arbeit!
+
+
+from libthermalraspi.sensors.simulation_sensorstub import SensorStub
+from libthermalraspi.sensors.storemock import StoreMock
+from libthermalraspi.sensors.services.parallelSampleCollector import ParallelSampleCollector
+
+import unittest
+
+class ParallelSampleCollectorTest(unittest.TestCase):
+    def test__parallel(self):
+        # Setup-Phase
+		# Liste mit Testmessungen
+		# measurements = [ 1, 2, 5, -9, -6 ]
+ 
+		# Sensor-Stub anlegen
+		# sensor = SensorStub(measurements)
+ 
+		# Store-Mock anlegen
+		# store = StoreMock()
+ 
+		# Dictionary aus Sensoren erzeugen (In diesem Fall nur ein Eintrag...)
+		# sensors = { "test-sensor1" : sensor  }
+ 
+ 
+		# Exercise-Phase:
+		# collector = ParallelSampleCollector(store, sensors)
+		#collector.run...
+ 
+ 
+ 
+		# Verify-Phase:
+		# Jetzt müssen alle Werte von measurements in store sein...
+		# Wenn ja -> Test OK
+
+#suite = unittest.defaultTestLoader.loadTestsFromTestCase(ParallelSampleCollectorTest)
+
+#if __name__ == '__main__':
+    #unittest.TextTestRunner().run(suite)
+    #pass
