7b607cb4bc3e077c7549a1aa4f172eb9ef6618d5
mkxsoft <mkxsoft@gmail.com>
2013-06-04 09:19:40 +0000

    SensorStub simuliert Temperaturmessungen.

---
+++ libthermalraspi/sensors/simulation_sensorstub.py
@@ -0,0 +1,22 @@
+#SensorStub simuliert die Temperaturmessungen und erhält einfach eine Liste 
+#mit Messwerten, die als Testeingangswerte dienen.
+
+from libthermalraspi.sensors.thermometer import Thermometer
+
+# SensorStub erhält eine Liste an Messwerten, die
+# in get_temperature zurück geliefert werden.
+# Dieser Test-Stub wird verwendet um im ParallelSampleCollector
+# Messungen zu simulieren.
+class SensorStub(Thermometer):
+    def __init__(self, measurements = []):
+        self.__measurements = measurements;
+        self.__index = 0
+    
+    def get_temperature(self):
+        self.__index = self.__index + 1
+        
+        if self.__index >= self.__measurements.length:
+            self.__index = 0
+            
+        return self.__measurements[self.__index]
+		
