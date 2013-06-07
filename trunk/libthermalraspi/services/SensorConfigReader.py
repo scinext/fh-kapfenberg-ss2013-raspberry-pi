efbd900e0d93e28071998879c7973a98c6c3f1d6
jf@faschingbauer.co.at <jf@faschingbauer.co.at@eaa47888-aff5-2b64-d939-c71d30150e1f>
2013-06-07 07:42:57 +0000

    remove sensordb.sqlite

--- trunk/libthermalraspi/services/SensorConfigReader.py
+++ trunk/libthermalraspi/services/SensorConfigReader.py
@@ -9,6 +9,7 @@ from libthermalraspi.sensors.ad7414 import AD7414Thermometer
 from libthermalraspi.sensors.tc74 import TC74Thermometer
 from libthermalraspi.sensors.hyt221 import Hyt221
 from libthermalraspi.sensors.simulation import CyclicThermometer
+from libthermalraspi.sensors.thermo_proxy_itmG2 import ThermoProxy_ItmG2
 
 class SensorConfigReader(object):
     def __init__(self, filePath ):
@@ -19,4 +20,6 @@ class SensorConfigReader(object):
                                                       ,'AD7414' : AD7414Thermometer
                                                       ,'TC74'   : TC74Thermometer
                                                       ,'Hyt221' : Hyt221
-                                                      ,'Cyclic' : CyclicThermometer } )
+                                                      ,'Cyclic' : CyclicThermometer
+                                                      ,'ProxyITMG2': ThermoProxy_ItmG2
+                                                      } )
